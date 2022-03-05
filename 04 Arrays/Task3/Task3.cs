25aa4070c9ac49df2dd7099cf741d5d005b8c151
alevenskiy <81563590+alevenskiy@users.noreply.github.com>
2022-03-05 16:45:03 +0300

    Add files via upload

---
+++ 04 Arrays/Task3/Task3.cs
@@ -0,0 +1,33 @@
+
+
+using System;
+class Task3 {
+  static void Main() {
+    int max = 0;
+    while (max <= 0){
+        Console.WriteLine("enter max of a value (only positive)");
+        max = int.Parse(Console.ReadLine());
+    }
+    
+    Random rand = new Random();
+    int answer = rand.Next(max);
+    bool flag = true;
+    Console.WriteLine("try to guess the Secret Number");
+    while (flag){
+        string quest = Console.ReadLine();
+        if(quest == ""){
+            flag = false;
+        }
+        else{
+            int value = int.Parse(quest);
+            if(value > answer) Console.WriteLine("" + value + " > number");
+            if(value < answer) Console.WriteLine("" + value + " < number");
+            if(value == answer) {
+                flag = false;
+                Console.Write("Yes, "); 
+            }
+        }
+    }
+    Console.WriteLine("Secret Number is " + answer);
+  }
+}

d6c209ae2ea6d216a13061eacf25b2daa252502e
Alexander Rozhdestvenskiy <ppcrn97@gmail.com>
2022-03-05 23:25:22 +0300

    add homework 4

---
+++ 04 Arrays/Task1/Task1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Task1
+{
+    static void Main()
+    {
+
+        int rows = 0;
+        int columns = 0;
+
+        while (rows <= 0)
+        {
+            Console.WriteLine("enter the number of rows");
+            rows = Convert.ToInt32(Console.ReadLine());
+        }
+        while (columns <= 0)
+        {
+            Console.WriteLine("enter the number of columns");
+            columns = Convert.ToInt32(Console.ReadLine());
+        }
+
+        long sum = 0;
+        int value = 0;
+        Random rand = new Random();
+
+        for (int i = 0; i < columns; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                value = rand.Next(int.MinValue, int.MaxValue);
+                Console.Write($"{value,15}");
+                sum += value;
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("" + sum);
+    }
+}
---
+++ 04 Arrays/Task2/Task2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Task2
+{
+    static void Main()
+    {
+        int size = 0;
+        while (size <= 0)
+        {
+            Console.WriteLine("enter size of a sequence");
+            size = int.Parse(Console.ReadLine());
+        }
+
+        int[] sequence = new int[size];
+        Console.WriteLine("enter " + size + " values");
+        for (int i = 0; i < size; i++)
+        {
+            sequence[i] = int.Parse(Console.ReadLine());
+        }
+
+        int min = sequence[0];
+        for (int i = 1; i < size; i++)
+        {
+            if (min > sequence[i])
+            {
+                min = sequence[i];
+            }
+        }
+        Console.WriteLine("min = " + min);
+    }
+}

8a00fd97e633bd739e3320c3f5860f1a53e3608a
Alexander Rozhdestvenskiy <ppcrn97@gmail.com>
2022-03-12 12:18:47 +0300

    add 5th homework

---
+++ 05 Sentences/Task1/Task1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Task1
+{
+    static string[] SplitSentences(string str)
+    {
+        return str.Split(' ');
+    }
+
+    static void Output(string[] str)
+    {
+        foreach (var sub in str)
+        {
+            Console.WriteLine(sub);
+        }
+    }
+
+    static void Main()
+    {
+        string str = Console.ReadLine();
+        Output(SplitSentences(str));
+    }
+}
---
+++ 05 Sentences/Task2/Task2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Task2
+{
+
+    static string[] SplitSentences(string str)
+    {
+        return str.Split(' ');
+    }
+
+    static string ReversWords(string input)
+    {
+        string[] words = SplitSentences(input);
+        string output = "";
+        for (int i = words.Length - 1; i >= 0; i--)
+        {
+            output += words[i] + " ";
+        }
+        return output;
+    }
+
+    static void Main()
+    {
+        string str = Console.ReadLine();
+        Console.WriteLine(ReversWords(str));
+    }
+}

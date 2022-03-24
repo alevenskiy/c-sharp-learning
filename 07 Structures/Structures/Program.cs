c16a111b269d371239747140d7c666336e597a53
al ro <ppcrn97@gmail.com>
2022-03-24 22:42:31 +0300

    edit 7th HW. Errors in input

--- 07 Structures/Structures/Program.cs
+++ 07 Structures/Structures/Program.cs
@@ -100,7 +100,7 @@ namespace Structures
         }
 
         /// <summary>
-        /// 4. Возвращение записи из файла для дальнейшего редактирования
+        /// 4. Взятие записи из файла для дальнейшего редактирования
         /// </summary>
         /// <param name="id">ID сотрудника в файле</param>
         /// <param name="filename">Путь до файла и его имя</param>
@@ -121,6 +121,98 @@ namespace Structures
                 }
                 Console.WriteLine("There is no Employee with this id");
                 return new Employee();
+            }
+        }
+
+        /// <summary>
+        /// 4. Редактирование записи из файла, для дальнейшего возврата в файл
+        /// </summary>
+        /// <param name="emp"></param>
+        /// <returns></returns>
+        static Employee EditEmployee(Employee emp)
+        {
+            while (true)
+            {
+                Console.WriteLine("\n" +
+                    "Enter 1 to change the Name\n" +
+                    "Enter 2 to change the Age\n" +
+                    "Enter 3 to change the Height\n" +
+                    "Enter 4 to change the Birth Date\n" +
+                    "Enter 5 to change the Birth City\n" +
+                    "Enter 0 to save changes and exit edit menu\n");
+                switch (Console.ReadLine())
+                {
+                    case "0":
+                        return emp;
+                    case "1":
+                        Console.WriteLine("Enter Name");
+                        emp.Name = Console.ReadLine();
+                        Console.WriteLine("Current Employee's data:\n" + emp.ToString(' '));
+                        break;
+                    case "2":
+                        Console.WriteLine("Enter Age");
+                        emp.Age = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Current Employee's data:\n" + emp.ToString(' '));
+                        break;
+                    case "3":
+                        Console.WriteLine("Enter Height");
+                        emp.Height = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Current Employee's data:\n" + emp.ToString(' '));
+                        break;
+                    case "4":
+                        Console.WriteLine("Enter Birth Date");
+                        emp.BirthDate = Convert.ToDateTime(Console.ReadLine());
+                        Console.WriteLine("Current Employee's data:\n" + emp.ToString(' '));
+                        break;
+                    case "5":
+                        Console.WriteLine("Enter Birth City");
+                        emp.BirthCity = Console.ReadLine();
+                        Console.WriteLine("Current Employee's data:\n" + emp.ToString(' '));
+                        break;
+                    default: break;
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// 4. Размещение записи в файле
+        /// </summary>
+        /// <param name="emp"></param>
+        /// <param name="filename"></param>
+        static void SetEmployeeInFile(Employee emp, string filename)
+        {
+            int size = 0;
+
+            if (File.Exists(filename))
+                size = getLastID(filename);
+            
+            Employee[] employees = new Employee[size];
+            int index = 0;
+
+            using(StreamReader sr = new StreamReader(filename))
+            {
+                string line;
+                while((line = sr.ReadLine()) != null)
+                {
+                    employees[index] = new Employee(line, '#');
+                    index++;
+                }
+            }
+            for(int i = 0; i < size; i++)
+            {
+                if(employees[index].Id == emp.Id)
+                {
+                    employees[index] = emp;
+                    break;
+                }
+            }
+            using (StreamWriter sw = new StreamWriter(filename, false))
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    sw.WriteLine(employees[i].ToString('#'));
+                }
             }
         }
 
@@ -208,16 +300,37 @@ namespace Structures
             return employees;
         }
 
+        /// <summary>
+        /// 6. Сохранение списка сотрудников в файл
+        /// </summary>
+        /// <param name="emps"></param>
+        /// <param name="filename"></param>
+        static void SaveEmpoyeesListIntoFile(Employee[] emps, string filename)
+        {
+            using (StreamWriter sw = new StreamWriter(filename, false))
+            {
+                foreach (Employee employee in emps)
+                {
+                    sw.WriteLine(employee.ToString('#'));
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             string filename = "list.txt";
 
-            #region last hw main
-            /*while (true)
+            while (true)
             {
-                Console.WriteLine("\nEnter 1 to show data\n" +
-                    "Enter 2 to enter data and add it to file\n" +
-                    "Enter 0 to close");
+                Console.WriteLine("\n" + 
+                    "Enter 1 to show list of Employees\n" +
+                    "Enter 2 to show Employee by ID\n" +
+                    "Enter 3 to Create Employee and add it to list\n" +
+                    "Enter 4 to Edit Employee by ID\n" +
+                    "Enter 5 to Delete Employee by ID\n" +
+                    "Enter 6 to show list of Employees by range of creation date\n" + 
+                    "Enter 7 to sort list of Employees\n" + 
+                    "Enter 0 to close\n");
                 string choice = Console.ReadLine();
 
                 if (choice == "0")
@@ -232,13 +345,74 @@ namespace Structures
                         break;
 
                     case "2":
-                        addLineToFile(enterData(), filename);
+                        Console.WriteLine("Enter ID");
+                        ShowEmployeeByID(Convert.ToInt32(Console.ReadLine()), filename);
+                        break;
+
+                    case "3":
+                        int id = getLastID(filename);
+                        AddEmployeeToFile(                              //2) добавляем запись в файл
+                            new Employee(   id.ToString() +             //1) создаем сотрудника из строки с ID
+                                      "#" + DateTime.Now.ToString() +   //      со временем создания
+                                      "#" + enterData(), '#'),          //      и с данными из ввода
+                            filename);
+                        break ;
+
+                    case "4":
+                        Console.WriteLine("Enter ID");
+                        SetEmployeeInFile(                  //5) ставим запись 
+                            EditEmployee(                   //4) редактируем запись
+                                GetEmployeeFromFile(        //3) берем запись
+                                    Convert.ToInt32(        //2) конвертим в int
+                                        Console.ReadLine()  //1) читаем ID записи
+                                        ), filename)
+                                ), 
+                            filename);
+                        break;
+
+                    case "5":
+                        Console.WriteLine("Enter ID");
+                        DeleteEmployeeFromFile(Convert.ToInt32(Console.ReadLine()), filename);
+                        break;
+
+                    case "6":
+                        Console.WriteLine("Enter Date from");
+                        DateTime from = Convert.ToDateTime(Console.ReadLine());
+                        Console.WriteLine("Enter Date to");
+                        DateTime to = Convert.ToDateTime(Console.ReadLine());
+
+                        Employee[] emps = GetEmployeesDateRange(from, to, filename);
+                        for(int i = 0; i < emps.Length; i++)
+                            Console.WriteLine(emps[i].ToString(' '));
+                        break;
+
+                    case "7":
+                        while (true)
+                        {
+                            Console.WriteLine("Enter 1 to ascending sort\n" +
+                                "Enter 2 to descending sort" +
+                                "Enter 0 to exit sort menu");
+                            string sortChoice = Console.ReadLine();
+                            if (sortChoice == "0")
+                                break;
+                            switch (sortChoice)
+                            {
+                                case "1":
+                                    SaveEmpoyeesListIntoFile(GetSortedEmployees(true, filename), filename);
+                                    break;
+
+                                case "2":
+                                    SaveEmpoyeesListIntoFile(GetSortedEmployees(false, filename), filename);
+                                    break;
+
+                                default: break;
+                            }
+                        }
                         break;
 
                     default: break;
                 }
-            }*/
-            #endregion
+            }
         }
 
         static int GetSizeList(string filename)
@@ -268,18 +442,23 @@ namespace Structures
         {
             int id = 0;
             string readLine;
-            using (StreamReader sr = new StreamReader(filename))
+            if (File.Exists(filename))
             {
-                while ((readLine = sr.ReadLine()) != null)
+                using (StreamReader sr = new StreamReader(filename))
                 {
-                    string[] splittedLine = readLine.Split('#');
-                    id = Convert.ToInt32(splittedLine[0]);
+                    while ((readLine = sr.ReadLine()) != null)
+                    {
+                        string[] splittedLine = readLine.Split('#');
+                        id = Convert.ToInt32(splittedLine[0]);
+                    }
                 }
             }
+            else
+                id = 1;
             return id;
         }
 
-        static void addLineToFile(string line, string fileName)
+        /*static void addLineToFile(string line, string fileName)
         {
             int id = 0;
             string date = DateTime.Now.ToString("dd.MM.yyyy HH:mm");
@@ -296,6 +475,7 @@ namespace Structures
                 sw.WriteLine(writeLine);
             }
         }
+        */
 
         static string enterData()
         {

14859fae2b4426d748e1b5c0f08672d8dd86021b
al ro <ppcrn97@gmail.com>
2022-04-04 17:21:59 +0300

    add 4th task of 8 hw

--- 08 Collections/Task4/Program.cs
+++ 08 Collections/Task4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Xml.Serialization;
+using System.Xml.Linq;
 
 namespace Task4
 {
@@ -6,7 +8,54 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            XElement person = new XElement("Person");
+
+            Console.WriteLine("input name");
+            string str = Console.ReadLine();
+            XAttribute name = new XAttribute("Name", str);
+            
+            person.Add(name);
+
+            XElement address = new XElement("Address");
+            XElement street = new XElement("Street");
+            XElement houseNumber = new XElement("HouseNumber");
+            XElement flatNumber = new XElement("FlatNumber");
+
+            Console.WriteLine("input street");
+            str = Console.ReadLine();
+            street.Add(str);
+            
+            Console.WriteLine("input house number");
+            str = Console.ReadLine();
+            houseNumber.Add(str);
+            
+            Console.WriteLine("input flat number");
+            str = Console.ReadLine();
+            flatNumber.Add(str);
+
+            address.Add(street);
+            address.Add(houseNumber);
+            address.Add(flatNumber);
+
+            XElement phones = new XElement("Phones");
+            XElement mobilePhone = new XElement("MobilePhone");
+            XElement flatPhone = new XElement("FlatPhone");
+
+            Console.WriteLine("input mobile phone number");
+            str = Console.ReadLine();
+            mobilePhone.Add(str);
+
+            Console.WriteLine("input flat phone number");
+            str = Console.ReadLine();
+            flatPhone.Add(str);
+
+            phones.Add(mobilePhone);
+            phones.Add(flatPhone);
+
+            person.Add(address);
+            person.Add(phones);
+
+            person.Save("_Person.xml");
         }
     }
 }
